c0dd1ee29778c4b67c492ebcba3759a235f03c10
Your name <Your email>
2025-02-09 17:45:18 +0100

    joueur1 connected

--- components/Joueur_1.js
+++ components/Joueur_1.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from '../styles/joueur1.module.css'
-import Image from 'react'
+import Image from 'next/image';
 
 function Joueur_1(props) {
 
@@ -8,11 +8,11 @@ function Joueur_1(props) {
   
   return (
     <div>
-      <div className={styles.joureur}>
+      <div className={styles.joueur}>
         <p> name of spell :{props.name}</p>
         <p> description of spell :{props.description}</p>
 
-    <Image src='../public/sorcier1.png' alt='avatar'/>
+    <Image src='/sorcier1.png' alt='avatar' width={600} height={314}/>
     <span className={styles.sort}></span>
     <button>Nouveau sorts</button>
     </div>
